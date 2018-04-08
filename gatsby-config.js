55248912f35a5d8d2a91649f6a050e2a14d335ba
daniel-prinsloo <dprinsloo@gmail.com>
2018-04-08 16:27:26 +0200

    Tutorial code as base for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+	pathPrefix: `/prototype`,
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+	`gatsby-plugin-netlify-cms`,
+	`gatsby-plugin-react-helmet`,
+	`gatsby-plugin-catch-links`,
+	{
+		resolve: `gatsby-source-filesystem`,
+		options: {
+			path: `${__dirname}/src/pages`,
+			name: 'pages',
+		}
+	},
+	`gatsby-transformer-remark`,
+  ],
 };
